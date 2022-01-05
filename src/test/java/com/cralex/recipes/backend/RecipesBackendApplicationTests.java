e9d34942e2fae3388098331264e68785492f93ac
crolfs47 <crolfs47@gmail.com>
2022-01-04 20:16:59 -0600

    Initial commit for a recipes app

---
+++ src/test/java/com/cralex/recipes/backend/RecipesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cralex.recipes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
